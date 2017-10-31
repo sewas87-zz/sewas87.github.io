2ee146270aa9a0db102ab911850ea3423b179665
sewas87 <hubman@tutanota.com>
2017-10-31 13:01:44 +0200

    -A

--- beta/js/common.js
+++ beta/js/common.js
@@ -1,3 +1,37 @@
 $(document).ready(function() { 
     $(".gallery").lightGallery();
+    $("#nav-toggle").on("click", function() {
+        $(this).toggleClass("active");
+        $('#nav').slideToggle();
+    });
+    $(".main-nav").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
+        $('#nav-toggle').toggleClass("active");
+        $('#nav').slideToggle();
+    });
+    $("#scrol-down").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 2000);
+    });
+   if (window.screen.width > 991){
+        new WOW().init();
+    }
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
 });
